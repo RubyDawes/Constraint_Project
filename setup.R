91111912d98f2fb5927b1430538b1659c6b669ec
RubyDawes <ruby.dawes@outlook.com>
2018-06-06 17:07:01 +1000

    added script for making 'universe_df' an amalgamation of all other script results ('making_universe_df')

--- setup.R
+++ setup.R
@@ -16,6 +16,9 @@ library(data.table)
 
 load("hgnc.table.rda")
 
-hgnc<- read.csv("Universe/Symbol_Check/gene_with_protein_product.txt",sep = "\t", comment.char = "#",stringsAsFactors = FALSE)
+hgnc<- read.csv("Gene_lists/Universe/gene_with_protein_product.txt",sep = "\t", comment.char = "#",stringsAsFactors = FALSE)
+hgnc$symbol<- checkGeneSymbols(hgnc$symbol,hgnc.table=hgnc.table)[[3]]
+universe_df <- data.frame(hgnc$hgnc_id,hgnc$symbol,hgnc$name,hgnc$gene_family,hgnc$mgd_id)
+names(universe_df) <- c("hgnc_id","gene","gene_name","gene_family","mgd_id")
 universe <- hgnc$symbol
 rm(hgnc)
