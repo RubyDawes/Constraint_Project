a89afc869d2de7784480c6ee17ced4cc23d44d44
RubyDawes <ruby.dawes@outlook.com>
2018-06-07 11:10:40 +1000

    updated readme

--- 2.ExAC_constraint/exac_constraint.R
+++ 2.ExAC_constraint/exac_constraint.R
@@ -1,6 +1,4 @@
-# a script to take omim genes vs NMD genes vs non-disease genes and compare constraint
-source("setup.R")
-
+# a script to get exac scores for protein coding genes
 #getting exac data
 #taking only needed columns
 exac <- read.table("Gene_lists/ExAC/fordist_cleaned_exac_r03_march16_z_pli_rec_null_data.txt",header = TRUE,fill = TRUE)
@@ -18,25 +16,6 @@ rm(a,b,baddups,genecheck)
 #removing exac genes that aren't protein coding
 exac <- exac[-which(!exac$gene%in%universe&!grepl(pattern="///",exac$gene)),]
 
-#get omim df
-source("1.OMIM_filtering/OMIM_parse_and_filter.R")
-
-#get NMD genes, check symbols
-nmd <- read.xlsx("Gene_lists/NMD_genes/nmd_genes_2018.xlsx")
-nmd$gene <- checkGeneSymbols(nmd$gene,unmapped.as.na=FALSE,hgnc.table=hgnc.table)[[3]]
-nmd$gene[which(nmd$gene=="Sep-09")]<- "SEPT9"
-#removing nmd genes that aren't protein coding
-nmd <- nmd[-which(!nmd$gene%in%universe&!grepl(pattern="///",nmd$gene)),]
-
-disease_genes <- append(omim$Gene ,setdiff(nmd$gene,omim$Gene),after=length(omim$Gene))
-
-#info on disease status of genes in exac
-exac$omim <- ifelse(exac$gene%in%omim$Gene,"Y","N")
-exac$omim_phen <- ifelse(exac$gene%in%omim$Gene,vlookup(exac$gene,omim,result_column="Phenotypes",lookup_column="Gene"),NA)
-exac$omim_inheritance <- ifelse(exac$gene%in%omim$Gene,vlookup(exac$gene,omim,result_column="Inheritance_pattern",lookup_column="Gene"),NA)
-
-exac$nmd <- ifelse(exac$gene%in%nmd$gene,"Y","N")
-exac$disease <- ifelse(exac$gene%in%disease_genes,"Y","N")
 
 
 
---
+++ Master.R
@@ -0,0 +1,36 @@
+#clears variables, loads dependencies, loads updated hgnc symbol table (for checking gene symbols), initialises universe_df 
+##which will be added to with info from other scripts, loads universe which will be used as reference list of 'all protein-coding genes'
+source("setup.R")
+
+#functions for making pretty graphs
+source("plot_functions.R")
+
+#get omim df with filtered list of OMIM genes with mim numbers, phenotypes, and inheritance
+##criteria for high confidence disease genes: phenotype mapping key (3) "molecular basis is known", filter out any nondiseases, 
+###susceptibilities, provisional links, somatic mutations, removing non protein-coding genes (noncoding RNAs, complex loci etc)
+source("1.OMIM_filtering/OMIM_parse_and_filter.R")
+
+#get list of NMD genes
+source("1.OMIM_filtering/nmd_genes.R")
+
+#get exac scores for protein coding genes
+source("2.ExAC_constraint/exac_constraint.R")
+
+#take all genes with mouse knockout/hypomorph phenotypes from MGI, see which ones are lethal and what the lethal phenotypes are
+##for list of lethal phenotypes see Gene_lists/MGI_MPs/MGI_lethal_phenotypes.xlsx
+###warning: pretty time consuming (takes about ~1.5 mins to run) recommend loading from output spreadsheet or rda
+#source("3.Mouse_phenotypes/MGI_parse.R")
+#mgi <- read.xlsx("output/spreadsheets/MGI_genes_with_phenotypes.xlsx)
+load("output/Data/mgi.rda")
+
+#df of genes tested in wang, blomen and hart CRISPR knockout papers and whether they were lethal or not in each of the 11 cell lines
+##tested throughout the three papers
+#source("4.Cell_Knockouts/cell_essential_genes.R")
+load("output/Data/cell_KOs.rda")
+
+#puts all info on disease status, phenotype, exac scores, mouse phenotypes and cell knockouts for each protein-coding gene in HGNC into one 
+##dataframe 'universe'
+source("5.Universe/making_universe_df.R")
+load("output/Data/universe_df.rda")
+
+
