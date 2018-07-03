fa85da8af0da2de9ab95b096fe1a53768841d751
RubyDawes <ruby.dawes@outlook.com>
2018-07-03 14:44:48 +1000

    fixed SELENOF gene in hgnc.table, added output spreadsheets folder

--- renew_HGNC_table/ renew_HGNC_table.R
+++ renew_HGNC_table/ renew_HGNC_table.R
@@ -3,4 +3,5 @@
 source(system.file("hgncLookup.R", package = "HGNChelper"))
 ##You should save this if you are going to use it multiple times,
 ##then load it from file rather than burdening HGNC's servers.
+hgnc.table$Approved.Symbol[which(hgnc.table$Approved.Symbol=="SEP15")]<-"SELENOF"
 save(hgnc.table, file="output/Data/hgnc.table.rda", compress="bzip2")
